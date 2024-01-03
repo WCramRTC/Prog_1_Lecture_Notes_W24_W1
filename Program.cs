def488841ca159e4a2d18df9a8b3e5c9a1fef975
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2024-01-02 23:27:47 -0800

    Updated

--- Program.cs
+++ Program.cs
@@ -2,8 +2,15 @@
 {
     internal class Program
     {
+
+        // Place all your code between the curly braces, { and }, of the Main block.
+        // You can see them indicated below by the arrows
+
         static void Main(string[] args)
-        { // <-----
+        { // <----- Place code in between here
+
+            // Console.WriteLine(); Is used to display text to the screen
+            // You must put the phrase you want to display in between double quotes, " "
             Console.WriteLine("Hello, Adam and Jake!");
             Console.WriteLine();
             Console.WriteLine("Popeye's Chicken Sandwich");
@@ -13,7 +20,7 @@
             // "Hello, Adam and Jake"
             // ;
 
-        } // <------
+        } // <------ and here
 
 
   
