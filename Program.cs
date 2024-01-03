6df7c640150a770dc86e471c554e05ef2ce04d20
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2024-01-02 19:51:40 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Prog_1_Lecture_Notes_W24_W1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { // <-----
+            Console.WriteLine("Hello, Adam and Jake!");
+            Console.WriteLine();
+            Console.WriteLine("Popeye's Chicken Sandwich");
+
+
+            // Console.WriteLine()
+            // "Hello, Adam and Jake"
+            // ;
+
+        } // <------
+
+
+  
+    } // class
+} // namespace
+
+
